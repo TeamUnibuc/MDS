c48156248c3828801c0e27b418692ad3615830c3
Theodor Moroianu <theodor.moroianu@gmail.com>
2021-02-24 17:08:21 +0200

    Add first link

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import { env } from './config';
+import express from 'express'
 
 export function sampleFunction (x: string): string {
   return x + x;
@@ -7,3 +8,14 @@ export function sampleFunction (x: string): string {
 console.log(`port env variable:  ${env.PORT}`);
 
 console.log(sampleFunction('something'));
+
+const app = express()
+
+app.get("/api", (req, res) => {
+  console.log(req.url)
+  res.json({"OK": "Yep"})
+})
+
+app.listen(env.PORT, () => {
+  console.log("Started to listen!")
+})
