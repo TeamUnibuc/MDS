83362286c574e46be15bfb7c7ef691b3441e196a
Theodor Moroianu <theodor.moroianu@gmail.com>
2021-02-25 14:17:43 +0200

    Link Frontend and Backend (#33)
    * Update venv
    
    * Add first link
    
    * Added environment variable setup for API_URL fror frontend
    
    * Added environment variable in env files
    
    * changed console log
    
    * Fix tests
    
    Co-authored-by: Felix Puscasu <puscasu.felix1@gmail.com>

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,6 +3,11 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
+  <div>
+    <p>
+      {{ message }}
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,8 +16,21 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
+  data() {
+    return {
+      message: ''
+    }
+  },
   components: {
     HelloWorld
+  },
+  async mounted() {
+    console.log("Got here!")
+    const response = await fetch('/api/', {
+      method: 'GET',headers: {'Content-Type': 'application/json'},
+    })
+    const realDeal = await response.json()
+    this.message = realDeal["OK"]
   }
 });
 </script>
