9861df57c9255b9f59845f1e4e26b489f399013b
Felix Puscasu <puscasu.felix1@gmail.com>
2021-03-04 12:44:18 +0000

    Properly kill old process and better check for npm dependencies

--- deploy_script.sh
+++ deploy_script.sh
@@ -6,9 +6,9 @@ conda activate MDS_env
 
 echo "========  !!!!   Installing npm/yarn dependencies"
 cd frontend
-yarn install
+yarn install --check-file
 cd ../backend
-yarn install
+yarn install --check-file
 
 echo "========  !!!!   Building for production"
 # Build the frontend
@@ -19,7 +19,7 @@ yarn build
 cd ../backend
 yarn build-prod
 
-echo "========  !!!!   Serving backend"
+echo "========  !!!!   Serving"
 echo "  ---     (frontend is static and should already be configured in nginx)"
 # Replace the served files with the new ones
 cd ../frontend
@@ -30,6 +30,7 @@ mv dist dist_nginx
 cd ../backend
 # Kill old process
 PORT=$(grep -oP '^PORT=\K.*' .env)
-kill $(lsof -t -i :${PORT})
+echo "Port is: ${PORT}"
+kill -9 $(lsof -t -i :${PORT})
 # Serve
 yarn serve
