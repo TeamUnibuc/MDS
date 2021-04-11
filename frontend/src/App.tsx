0b73c121bed19e6f54efa48e47bb654d11d7ce93
Constantin <56653779+ConstantinDragancea@users.noreply.github.com>
2021-04-11 21:14:35 +0300

    Migrate to react (#43) by Costel
    * initial commit
    
    * update integration tests for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// import { Redirect, Switch, Route, useParams } from "react-router-dom";
+
+function App() {
+
+  const [msg, setMsg] = useState('')
+  
+  // on mount basically
+  // the function in useEffect cannot be async, so we trick it
+  useEffect(() => {
+    const func = async () => {
+      const my_req = await fetch('/api', {
+        method: 'GET',headers: {'Content-Type': 'application/json'},
+      })
+  
+      const resp = await my_req.json()
+      setMsg(resp["OK"])
+    }
+    func();
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <strong>
+          {msg}
+        </strong>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          learn react
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/mockfetch.test.js
@@ -0,0 +1,48 @@
+// Taken from https://reactjs.org/docs/testing-recipes.html
+
+// user.test.js
+
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+// import User from "./user";
+import App from './App';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders user data", async () => {
+    const fakeResponse = {
+        "OK": "fake response from server"
+    };
+  
+    jest.spyOn(global, "fetch").mockImplementation(() => 
+        Promise.resolve({
+            json: () => Promise.resolve(fakeResponse)
+        })
+    );
+
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+        render(<App/>, container);
+    });
+
+    // expect(container.querySelector("summary").textContent).toBe(fakeUser.name);
+    // expect(container.querySelector("strong").textContent).toBe(fakeUser.age);
+    // expect(container.textContent).toContain(fakeUser.address);
+    expect(container.querySelector("strong").textContent).toContain(fakeResponse["OK"]);
+
+    // remove the mock to ensure tests are completely isolated
+    global.fetch.mockRestore();
+});
