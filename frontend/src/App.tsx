884a7abdff7adbc8a537a99668f5122adbe94ed1
ConstantinDragancea <dragancea.constantin@gmail.com>
2021-04-16 12:55:05 +0300

    test and deploy script fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
 import { DefaultPage, TestEval } from './pages';
-import { Switch, Route } from'react-router-dom'
+import { Switch, Route, BrowserRouter as Router } from'react-router-dom'
 
 function App() {
 
   return (
-    <Switch>
-      <Route key="default" path="/" exact component={DefaultPage}/>
-      <Route key="testeval" path="/testeval" exact component={TestEval}/>
-    </Switch>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route key="default" path="/" exact component={DefaultPage}/>
+        <Route key="testeval" path="/testeval" exact component={TestEval}/>
+      </Switch>
+    </Router>
   );
 }
 
