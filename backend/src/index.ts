cbea6e38d6b6a5298f1509267c5cab887ab3cedb
Theodor Moroianu <theodor.moroianu@gmail.com>
2021-04-29 12:07:43 +0300

    Refactor Engine connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,66 +1,58 @@
 import { env } from './config'
 import express from 'express'
-import zerorpc from 'zerorpc'
 import { startMongoConnection } from './DBConnection'
+import { EngineConnection } from './EngineConnection'
 
 // initialize connection to database
 startMongoConnection()
 
 
 //  Start express erver to listen for http api requests
-
 const app = express()
 
 // Automatically get the json content of the request body
 app.use(
-  express.urlencoded({
-    extended: true
-  })
+    express.urlencoded({
+        extended: true
+    })
 )
 
 app.use(express.json())
 
-// Connect to the engine.
-const client = new zerorpc.Client();
-client.connect("tcp://127.0.0.1:4242");
 
 app.post("/api/fight", (req, res) => {
-  console.log("Received a fight request at " + req.url)
-  const engine = req.body.engine;
-  const bots = req.body.bots;
+    console.log("Received a fight request at " + req.url)
+    const engine = req.body.engine;
+    const bots = req.body.bots;
 
-  console.log("Content of the request")
-  console.log("Engine: " + engine + "\nvars: " + bots)
-  
-  const obj = {
-    engine: engine,
-    bots: bots
-  }
-  const obj_string = JSON.stringify(obj)
-  client.invoke("StartSimulation", obj_string, function(err: string, result: string) {
-    result = JSON.parse(result)
-    console.log(result)
-    res.json(result)
-  });
+    console.log("Content of the request")
+    console.log("Engine: " + engine + "\nvars: " + bots)
+
+    EngineConnection.Fight(engine, bots)
+        .then(result => res.json(result))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(403);
+        })
 })
 
 app.get("/api", (req, res) => {
-  console.log(req.url)
-  res.json({"OK": "Yep"})
+    console.log(req.url)
+    res.json({ "OK": "Yep" })
 })
 
 const instance = app.listen(env.PORT, () => {
-  console.log(`URL: http://localhost:${env.PORT}`)
-  console.log(`Running backend in ${env.NODE_ENV} environment`)
+    console.log(`URL: http://localhost:${env.PORT}`)
+    console.log(`Running backend in ${env.NODE_ENV} environment`)
 })
 
 export const closeServer = (): void => {
-  instance.close()
-  client.close()
+    instance.close();
+    EngineConnection.CloseConnection();
 }
 
 // idiot export
 
 export const sampleFunction = (x: string): string => {
-  return x + x
+    return x + x
 }
