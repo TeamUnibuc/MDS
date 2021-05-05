9c078367de028b888e6c8cd75af41b4fed6e82ea
Theodor Moroianu <theodor.moroianu@gmail.com>
2021-05-05 10:21:10 +0300

    Fix missing package

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Header(props: {authenticated: boolean, handleNotAuth(): void}): JSX.Element {
     const handleLoginClick = () => {
